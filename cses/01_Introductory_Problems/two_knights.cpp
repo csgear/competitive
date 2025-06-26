8da0aab43fb73b1e5d8f87acb66bf28e8ddd52ad
csgear <csgear@live.com>
2025-06-26 13:42:31 -0400

    2 more luogu

--- cses/01_Introductory_Problems/two_knights.cpp
+++ cses/01_Introductory_Problems/two_knights.cpp
@@ -3,23 +3,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio                                                                 \
-  ios_base::sync_with_stdio(false);                                            \
-  cin.tie(NULL);                                                               \
-  cout.tie(NULL);
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
 
 #define ll long long
 
 int main() {
-  fastio;
+    fastio;
 
-  int n;
-  cin >> n;
+    int n;
+    cin >> n;
 
-  for (long long k = 1; k <= n; k++) {
-    ll total = (k * k * (k * k - 1)) / 2;
-    ll attack = 4LL * (k - 1) * (k - 2);
-    cout << total - attack << endl;
-  }
-  return 0;
+    for (long long k = 1; k <= n; k++) {
+        ll total = (k * k * (k * k - 1)) / 2;
+        ll attack = 4LL * (k - 1) * (k - 2);
+        cout << total - attack << endl;
+    }
+    return 0;
 }
