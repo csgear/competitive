5e4e911725e3b2cc3e825b7a6f0244abc2ea773c
csgear <csgear@live.com>
2025-10-11 18:58:28 -0400

    Add solution for Acwing problem 1128 using Dijkstra's algorithm

---
+++ acwing/3. Graph/p1126.cpp
@@ -0,0 +1,45 @@
+// https://www.acwing.com/problem/content/1128/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2010;
+
+int n, m, s, t;
+double w[N][N];
+double dist[N];
+bool st[N];
+
+void dijkstra() {
+    dist[s] = 1.0;
+    for (int i = 1; i <= n; i++) {
+        int t = -1;
+        for (int j = 1; j <= n; j++) {
+            if (!st[j] && (t == -1 || dist[t] < dist[j])) t = j;
+        }
+
+        st[t] = true;
+        for (int j = 1; j <= n; j++) dist[j] = max(dist[j], dist[t] * w[t][j]);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> m;
+    while (m--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        double d = (100.0 - c) / 100;
+        w[a][b] = w[b][a] = max(w[a][b], d);
+    }
+
+    cin >> s >> t;
+
+    dijkstra();
+
+    cout << fixed << setprecision(8) << 100.0 / dist[t] << endl;
+
+    return 0;
+}
---
+++ acwing/3. Graph/p1126_1.cpp
@@ -0,0 +1,73 @@
+// https://www.acwing.com/problem/content/1128/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2010;
+const int M = 2e5 + 10;
+
+struct Edge {
+    int to, nxt;
+    double w;
+} edges[M];
+
+int head[N], cnt;
+double dist[N];
+bool visited[N];
+int n, m, s, t;
+
+void add_edge(int u, int v, double w) {
+    edges[cnt] = {v, head[u], w};
+    head[u] = cnt++;
+}
+
+void dijkstra() {
+    dist[s] = 1.0;
+    priority_queue<pair<double, int>> pq;
+    pq.push({1.0, s});
+
+    while (!pq.empty()) {
+        auto [prob, u] = pq.top();
+        pq.pop();
+
+        if (visited[u]) continue;
+        visited[u] = true;
+
+        if (u == t) break;
+
+        for (int i = head[u]; i != 0; i = edges[i].nxt) {
+            int v = edges[i].to;
+            double w = edges[i].w;
+            if (dist[u] * w > dist[v]) {
+                dist[v] = dist[u] * w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    cnt = 1;
+    for (int i = 1; i <= n; i++) head[i] = 0;
+
+    while (m--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        double prob = (100.0 - c) / 100.0;
+        add_edge(a, b, prob);
+        add_edge(b, a, prob);
+    }
+
+    cin >> s >> t;
+
+    dijkstra();
+
+    cout << fixed << setprecision(8) << 100.0 / dist[t] << endl;
+
+    return 0;
+}
