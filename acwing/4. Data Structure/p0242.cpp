11c7f1401dd933de93413c58c103b7c271ea2b7a
csgear <csgear@live.com>
2025-10-29 19:00:32 -0400

    Add solution for problem 248 using BIT and difference array Implement range add and point query functionality in p0242.cpp

---
+++ acwing/4. Data Structure/p0242.cpp
@@ -0,0 +1,58 @@
+// https://www.acwing.com/problem/content/248/
+// BIT + difference array + sum
+// range add + point query
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const int N = 100010, M = 100010;
+
+int a[N];
+ll tree[N * 2];
+
+int n, m;
+
+int lowbit(int x) { return x & -x; }
+
+void add(int x, int c) {
+    for (int i = x; i <= n; i += lowbit(i)) {
+        tree[i] += c;
+    }
+}
+
+ll query(int x) {
+    ll res = 0;
+    for (int i = x; i; i -= lowbit(i)) {
+        res += tree[i];
+    }
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        add(i, a[i] - a[i - 1]);
+    }
+
+    for (int i = 1; i <= m; i++) {
+        char op;
+        cin >> op;
+        if (op == 'Q') {
+            int x;
+            cin >> x;
+            cout << query(x) << "\n";
+        } else {
+            int l, r, c;
+            cin >> l >> r >> c;
+            add(l, c);
+            add(r + 1, -c);
+        }
+    }
+
+    return 0;
+}
---
+++ acwing/4. Data Structure/p0243.cpp
@@ -0,0 +1,64 @@
+// https://www.acwing.com/problem/content/244/
+// 2 BIT trees + difference array + prefix sum
+// range add + range sum
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const int N = 100010, M = 100010;
+
+int a[N];
+ll tr1[N * 2];
+ll tr2[N * 2];
+
+int n, m;
+
+int lowbit(int x) { return x & -x; }
+
+void add(ll tr[], int x, ll c) {
+    for (int i = x; i <= n; i += lowbit(i)) {
+        tr[i] += c;
+    }
+}
+
+ll query(ll tr[], int x) {
+    ll res = 0;
+    for (int i = x; i; i -= lowbit(i)) {
+        res += tr[i];
+    }
+    return res;
+}
+
+ll prefix_sum(int x) { return query(tr1, x) * (x + 1) - query(tr2, x); }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        add(tr1, i, a[i] - a[i - 1]);
+        add(tr2, i, (ll)(a[i] - a[i - 1]) * i);
+    }
+
+    for (int i = 1; i <= m; i++) {
+        char op;
+        cin >> op;
+        if (op == 'Q') {
+            int l, r;
+            cin >> l >> r;
+            cout << prefix_sum(r) - prefix_sum(l - 1) << "\n";
+        } else {
+            int l, r, c;
+            cin >> l >> r >> c;
+            add(tr1, l, c);
+            add(tr1, r + 1, -c);
+            add(tr2, l, (ll)l * c);
+            add(tr2, r + 1, (ll)(r + 1) * -c);
+        }
+    }
+
+    return 0;
+}
